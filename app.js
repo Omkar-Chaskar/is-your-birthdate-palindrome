4e0981cc1a7b61b1e4180ad91f0a063193db90fb
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-08-05 13:46:17 +0530

    updating next palindrome

--- app.js
+++ app.js
@@ -25,7 +25,37 @@ btnCheck.addEventListener("click",function (){
             msg.style.display ="block";
         }else{
             console.log("not palindrome")
+            nextPalindromeDate();
         }
     },3000)
      
 }) 
+
+
+function nextPalindromeDate(arrDate){
+    let day = "";
+    let month = "";
+    let year = "";
+    let reversedSumArr = "";
+    day === arrDate[2];
+    month === arrDate[1];
+    year === arrDate[0];
+    const monthDays =[31 ,28 ,31, 30, 31, 30, 31, 30, 31, 30, 31, 30]
+
+    let nextSum = function (day,month,year){
+        let sumArr = day + month;
+        for(let i=sumArr.length-1 ;i>=0 ;i--){
+            reversedSumArr += sumArr[i];
+        }
+    }
+    do{
+        for(let j=0; j>monthDays.length; j++){
+            Number(day)++;
+            if(day > monthDays[j]){
+                month = Number(month) + 1;
+            }
+            console.log(month)
+        }
+    }
+    while(year == reversedSumArr)
+}
